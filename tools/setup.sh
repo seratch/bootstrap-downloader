b78c6be37f518b9d61dc7702bd0e96f8905fc073
Kazuhiro Sera <seratch@gmail.com>
2012-05-30 23:53:39 +0900

    Udpated bash_profile when setting up

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/sh 
 
 BIN=${HOME}/bin
 mkdir -p ${BIN}
 COMMAND=${BIN}/bootstrap
 curl https://raw.github.com/seratch/bootstrap-downloader/master/tools/bootstrap > ${COMMAND}
 chmod +x ${COMMAND}
+
+if [ ! `grep 'PATH=${PATH}:${HOME}/bin' ${HOME}/.bash_profile` ]; then
+  echo "PATH=\${PATH}:\${HOME}/bin" >> ${HOME}/.bash_profile
+fi
 
 echo "
 command installed to ${BIN}/bootstrap
